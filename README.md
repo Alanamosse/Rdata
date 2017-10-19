da723e2bcc2fc45228b98a1860324a917782c564
Alanamosse <alan.amosse@gmail.com>
2017-10-19 14:35:30 +0200

    minor changes, add comments

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # RData reader for Galaxy
 This tool first reads a RData file thenn extracts the chosen elements to single outputs.
 
+## TODO
+The tool should indicates when the Rdata file doesn't match the attriutes(s) selected.
+Can happen when multiple files are avaible and the user doesn't select the good one.
+
 ## Known errors
 * Execution fail when the variable to extract return a null value
 	* Resolved
--- rdata_extractor.R
+++ rdata_extractor.R
@@ -1,4 +1,7 @@
 #!/usr/bin/env Rscript
+#Use a Rdata file and attributes to extract
+#Get every argument and write a file with its values(s)
+
 
 #get the rdata file
 args = commandArgs(trailingOnly=TRUE)
@@ -8,26 +11,26 @@ sum<-summary(rdata)
 
 #get the selected attributes to explore
 attributes_selected <- commandArgs(trailingOnly=TRUE)[2]
+attributes<-strsplit(attributes_selected, ",") #List of elements
 
-attributes<-strsplit(attributes_selected, ",")
 len<-length(attributes[[1]])
 
-write.table(sum,file = "output.tsv")
+write.table(sum,file = "summary.tsv")
 
 for (i in 1:len){
-	attribute<-attributes[[1]][i]
-	file<-paste(attributes[[1]][i],".tsv",sep="")
+	attribute<-attributes[[1]][i] #Get the attribute i 
+	file<-paste(attributes[[1]][i],".tsv",sep="") #Filename definition
 
-	attribute_val<-eval(parse(text=paste("rdata$",attribute,sep="")))
+	attribute_val<-eval(parse(text=paste("rdata$",attribute,sep=""))) #Extract the value(s)
 
-	if(is.null(attribute_val)){
+	if(is.null(attribute_val)){ #Galaxy can't produce output if NULL
 		write.table("Return NULL value",file = file)
-		next
+		next #Exit loop
 	}
 
-	if (typeof(attribute_val)=="list") {
+	if (typeof(attribute_val)=="list") { #Need to be corrected, fail in galaxy but not in R
 		attribute_val<-as.data.frame(do.call(rbind, attribute_val))
-	}else if ( typeof(attribute_val)=="language") {
+	}else if ( typeof(attribute_val)=="language") { #OK
 		attribute_val<-toString(attribute_val,width = NULL)
 		write.table(attribute_val,file = file)
 		next
