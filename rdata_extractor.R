b25cf6df8770ee53ae70ff387ab29245829218ca
Alanamosse <alan.amosse@gmail.com>
2017-10-25 11:43:23 +0200

    Multiple file types implemented

--- rdata_extractor.R
+++ rdata_extractor.R
@@ -22,17 +22,18 @@ for (i in 1:len){
 		error<-paste(attribute, " doesn't exist in the RData. Check the inputs files")
 		write(error, stderr())
 	}
-	file<-paste(attributes[[1]][i],".tsv",sep="") #Filename definition
 
 	attribute_val<-eval(parse(text=paste("rdata$",attribute,sep=""))) #Extract the value(s)
 
 	if(is.null(attribute_val)){ #Galaxy can't produce output if NULL
-		write.table("Return NULL value",file = file)
+		file<-paste(attributes[[1]][i],".txt",sep="") #Filename definition
+		write("Return NULL value",file = file)
 		next #Exit loop
 	}
 
 	if (typeof(attribute_val)=="list"){ #Need to be corrected, fail in galaxy but not in R
 		if(length(attribute_val)=="0"){
+			file<-paste(attributes[[1]][i],".txt",sep="") #Filename definition
 			sink(file=file)
 			print("Empty list :") #If the list is empty without element, file is empty and an error occur in galaxy
 			print(attribute_val)
@@ -40,10 +41,16 @@ for (i in 1:len){
 			next
 		}else{
 			attribute_val<-as.data.frame(do.call(rbind, attribute_val))
+			file<-paste(attributes[[1]][i],".tsv",sep="") #Filename definition
+			write.table(attribute_val,file = file)
+			next
 		}
 	}else if (typeof(attribute_val)=="language"){ #OK
 		attribute_val<-toString(attribute_val,width = NULL)
+		file<-paste(attributes[[1]][i],".txt",sep="") #Filename definition
+		write(attribute_val,file = file)
+		next
 	}
-	write.table(attribute_val,file = file)
-
+	file<-paste(attributes[[1]][i],".tsv",sep="") #Filename definition
+	write.table(attribute_val,file = file)	
 }
